08c76a6b3feb6e79e42f65ee59626f32445d74e7
Crixxtian <cristiandmachado123@gmail.com>
2023-10-05 11:44:17 -0300

    terminado de fazer filtros

--- src/pages/Cardapio/Filtros/index.tsx
+++ src/pages/Cardapio/Filtros/index.tsx
@@ -8,13 +8,29 @@ import filtros from './filtros.json'
 
 type IOpicao = typeof filtros[0];
 
-export default function Filtros(){
+interface Props{
+    filtro: number | null,
+    setFiltro: React.Dispatch<React.SetStateAction<number | null>>
+}
+
+export default function Filtros({filtro, setFiltro}: Props){
+    function selecionarFiltros(opcao: IOpicao){
+        return setFiltro(opcao.id);
+    }
 
     return(
         <div className={styles.filtro}>
             {filtros.map((opcao) => (
-                <button className={styles.filtro__filtro} key={opcao.id}>
-                    {opcao.label}
+                <button 
+                className={`
+                    ${styles.filtro__filtro}
+                    ${filtro == opcao.id ? styles["filtro__filtro--ativo"] : ""}
+                `
+                } 
+                key={opcao.id}
+                onClick={()=> selecionarFiltros(opcao)}
+                >
+                {opcao.label}
                 </button>
             ))}
         </div>
